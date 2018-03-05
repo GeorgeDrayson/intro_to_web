78ce3510f733587f696339fe3c23f5deb33d4d01
George Drayson <drayson.g@mac.com>
2018-03-05 18:20:34 +0000

    Added parameters and a form to set the paramaters

--- app.rb
+++ app.rb
@@ -3,13 +3,21 @@ require 'sinatra'
 name = "Hello"
 
 get '/' do
-  name
+  params[:name]
 end
 
 get '/secret' do
   'Subscribe to Ricky'
 end
 
-get '/cat' do
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  @style = 'border-style:dashed;border-color:' + params[:color].to_s
   erb(:index)
 end
